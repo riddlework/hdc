a594572f4f0c40966a84184f477936dc6c70f35b
cslinxs <cslinxs@DN0a226737.SUNet>
2024-07-26 17:04:36 -0700

    update to branch pred

--- hdc-branch-pred.py
+++ hdc-branch-pred.py
@@ -3,23 +3,26 @@ from rev_list import *
 import csv
 import tqdm
 import matplotlib.pyplot as plt
-from pprint import pprint
 
 class branchPredictor:
     
-    def __init__(self,k):
+    def __init__(self,history,k=3):
         # initialize size of k-grams
         self.k = k
         
         # initialize atomics -- decisions 
         self.decisions = HDCodebook()
-        self.decisions.add("y")
-        self.decisions.add("n")
+        self.decisions.add("0")
+        self.decisions.add("1")
+
+        self.history = history
+        self.history_vecs = self.list_to_vec(history)
+        self.unthresholded = []
 
 
-    # takes a list of decisions and returns a list of their representative hypervectors
+    # convert list of decisions to list of representative vectors
     def list_to_vec(self,decision_list):
-        return [self.decisions.get(decision) for decision in decision_list]
+        return [self.decisions.get(str(decision)) for decision in decision_list]
     
 
     # create gram out of given list of decision vectors 
@@ -29,15 +32,12 @@ class branchPredictor:
 
     # assumes that history is long enough for k-gram
     # create history vector by encoding past branch decisions
-    def encode_history(self,history):
+    def encode_history(self,i):
         
-        # retrieve list of representative hypervectors
-        history_vecs = self.list_to_vec(history) 
-
         # create list of bound k-grams
         grams = []
-        for i in range(len(history_vecs)-self.k+1): 
-            run = history_vecs[i:i+self.k]
+        for j in range(i-self.k+1):
+            run = self.history_vecs[j:j+self.k]
             grams.append(self.encode_run(run))
 
         # bundle together and return 
@@ -46,13 +46,12 @@ class branchPredictor:
 
     # assumes that history is long enough for k-gram
     # create query vector from last k-1 items from memory
-    def make_query(self,history):
+    def make_query(self,i):
+
+        lenh = len(self.history_vecs)
 
         # retrieve last k-1 items from history
-        query = history[len(history)-self.k+1:len(history)]
-
-        # convert to hypervectors
-        query_vecs =  self.list_to_vec(query)
+        query_vecs = self.history_vecs[lenh-self.k+1:lenh]
 
         # encode run
         run = self.encode_run(query_vecs)
@@ -63,37 +62,90 @@ class branchPredictor:
 
     # predict next branch outcome based on current decision vector
     def predict(self,history_hv,query_hv):
-        return self.decisions.wta(HDC.bind(history_hv,query_hv))
+        return int(self.decisions.wta(HDC.bind(history_hv,query_hv)))
+
+    # test predictor
+    def test(self,plot=True):
+
+        if plot == True: 
+            print("======= testing predictor ======")
+
+        # initialize parameters
+        correct = 0
+        accuracies = []
+
+        for i in (pbar := tqdm.tqdm(range(len(self.history)))):
+
+            # if history is not long enough for k-gram, make random prediction
+            if i < self.k:
+                rand_num = np.random.rand()
+                if rand_num < 0.5:
+                    prediction = 1 
+                else:
+                    prediction = 0 
+
+            # otherwise, make prediction based on history
+            else:
+                # make history and query vectors
+                history_hv = self.encode_history(i)  
+                query_hv = self.make_query(i)
+                
+                # generate prediction and compare to actual 
+                prediction = self.predict(history_hv,query_hv)
+
+            # update accuracy using actual decision
+            actual = self.history[i]
+
+            if prediction == actual:
+                correct += 1
+
+            accuracy = float(correct) / (i+1)
+            accuracies.append(accuracy)
+            pbar.set_description("accuracy=%f" % accuracy)
+
+        # plot accuracies 
+        if plot == True:
+            make_plot(func=0,accuracies=accuracies)
+
+            # print final accuracy
+            print(f"ACCURACY: {accuracy}")
+
+        else:
+            return accuracies
+
 
 
 def initialize(k=3):
     # initialize data 
     HDC.SIZE = 10000
 
+    # initialize decisions
     decisions = []
     with open("./data/traces/410185-dataset.txt","r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         #decisions = [row["decision"] for row in csv_reader]
 
         for row in csv_reader:
-            decisions.append(row["decision"])
+            decisions.append(int(row["decision"]))
             
             # restrict amount of computation
-            if len(decisions) >= 100:
+            if len(decisions) >= 2500:
                 break
 
-    # reverse decision array to get most recent decisions first
-    decisions.reverse()
+    # convert into numpy list
+    decisions = np.array(decisions)
+
+    # reverse decisions array to get most recent decisions first
+    decisions = np.flip(decisions)
 
     # initialize branch predictor
-    predictor = branchPredictor(k)
+    predictor = branchPredictor(decisions,k=k)
 
-    # reverse decision array to get most recent decisions first
-    return decisions,predictor
+    return predictor
 
 
 # test different k-gram sizes
-def test_k_gram_sizes(history,predictor,k_vals=[i for i in range(3,10)]):
+def test_k_gram_sizes(predictor,k_vals=[i for i in range(3,10)]):
 
     print("======= testing k-gram sizes ======")
 
@@ -102,7 +154,7 @@ def test_k_gram_sizes(history,predictor,k_vals=[i for i in range(3,10)]):
         
         print(f"======= testing predictor k={val} ======")
         predictor.k = val
-        all_accuracies[val] = test_predictor(history,predictor,plot=False)
+        all_accuracies[val] = predictor.test(plot=False)
 
     # print each final accuracy
     for k,accuracies in all_accuracies.items():
@@ -110,56 +162,9 @@ def test_k_gram_sizes(history,predictor,k_vals=[i for i in range(3,10)]):
 
     # plot accuracies
     make_plot(func=1,all_accuracies=all_accuracies)
-
-
-def test_predictor(history,predictor,plot=True):
-
-    if plot == True: 
-        print("======= testing predictor ======")
-
-    # initialize parameters
-    correct = 0
-    accuracies = []
-
-    for i in (pbar := tqdm.tqdm(range(len(history)))):
-
-        # if history is not long enough for k-gram, make random prediction
-        if i < predictor.k:
-            rand_num = np.random.rand()
-            if rand_num < 0.5:
-                prediction = "y"
-            else:
-                prediction = "n"
-
-        # otherwise, make prediction based on history
-        else:
-            # make history and query vectors
-            history_hv = predictor.encode_history(history[:i])  
-            query_hv = predictor.make_query(history[:i])
-            
-            # generate prediction and compare to actual 
-            prediction = predictor.predict(history_hv,query_hv)
-         
-        # update accuracy using actual decision
-        actual = history[i]
-
-        if prediction == actual:
-            correct += 1
-
-        accuracy = float(correct) / (i+1)
-        accuracies.append(accuracy)
-        pbar.set_description("accuracy=%f" % accuracy)
-
-    # plot accuracies 
-    if plot == True:
-        make_plot(func=0,accuracies=accuracies)
-
-        # print final accuracy
-        print(f"ACCURACY: {accuracy}")
-    else:
-        return accuracies
         
 
+# generate plot based on data 
 def make_plot(func=0,accuracies=[],all_accuracies={}):
     
     # one-line plot
@@ -234,13 +239,13 @@ def main():
     k = 3
 
     # initialize data
-    history,predictor = initialize(k)
+    predictor = initialize(k)
     
     # test different k-gram sizes
     #test_k_gram_sizes(history,predictor)
 
     # test predictor
-    test_predictor(history,predictor)
+    predictor.test()
 
     # debugg/testing simple code 
     # debug_testing(predictor)
